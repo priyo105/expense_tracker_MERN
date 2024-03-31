b8056abc9e1e7c9dcdd3fa9ead8a8e2ec0beab8c
priyo105 <adnankamal972@gmail.com>
2024-03-31 11:47:15 +0100

    experiment

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,11 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://wise-expensetracker.netlify.app",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://wise-expensetracker.netlify.app",
+//   })
+// );
 app.options("*", cors());
 
 app.use(morgan("tiny"));
