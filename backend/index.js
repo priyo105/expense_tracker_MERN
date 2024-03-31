b14db78d444fe590231182b47c455c92b764b53f
priyo105 <adnankamal972@gmail.com>
2024-03-31 11:18:34 +0100

    jani na

--- backend/index.js
+++ backend/index.js
@@ -18,28 +18,7 @@ const NormalAuthinticate = require("./src/Routes/NormalAuthinticate.js");
 
 const corsOptions = {
   origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
 };
-
-// CORS allow function
-function allowCors(req, res, next) {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-    return;
-  }
-
-  next();
-}
 
 app.use("/uploads", express.static("uploads"));
 
